8dcde832cf1ec20e0f1e193fe93532ed1da8e844
OsvaldoReTo <resendiz.tolentino.osvaldo@gmail.com>
2022-09-29 15:20:37 -0500

    Se realizó ejercicio 1 live-coding-1

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // Escribe tu código aquí.
+const lista = document.getElementById("lista")
+const btnLista = document.getElementById("btn")
+
+myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio']
+
+btnLista.addEventListener("click", function(e){
+    e.preventDefault();
+    bigWords("bocina", myArray);
+
+})
+
+function bigWords(palabra, arr) {
+        let output =[];
+        arr.forEach(element => {
+            if(element.length>palabra.length){
+                output.push(element);
+                console.log(output);
+            }
+        }); 
+        lista.innerHTML+=`<li>${output[0]}</li><li>${output[1]}<li> ${output[2]}</li>`  
+}
